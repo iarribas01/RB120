e13bcaafdac3f1e6b420042c4f2fe886cfdd5f58
Isabel <iarribas01@gmail.com>
2022-05-05 11:24:04 +0100

    Finished OO Basics: Classes and objects 2 exercises

--- Exercises/Classes_and_Objects_2/identify_yourself.rb
+++ Exercises/Classes_and_Objects_2/identify_yourself.rb
@@ -1,21 +1,38 @@
 =begin 
 Covering questions:
   identify yourself (part 1)
-
+  identify yourself (part 2)
 =end
 
 
+# class Cat
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def identify
+#     self
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# p kitty.identify
+
+#######################
+
 class Cat
-  attr_accessor :name
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
-  def identify
-    self
+  def to_s
+    "I'm #{name}!"
   end
 end
 
 kitty = Cat.new('Sophie')
-p kitty.identify
+puts kitty
