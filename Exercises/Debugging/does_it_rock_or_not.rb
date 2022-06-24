f32d7407a2051343f90363f4fdd86d524f261eaa
Isabel <iarribas01@gmail.com>
2022-06-24 21:49:53 +0100

    Stuck on last debugging problem

--- Exercises/Debugging/does_it_rock_or_not.rb
+++ Exercises/Debugging/does_it_rock_or_not.rb
@@ -1,3 +1,73 @@
+# class AuthenticationError < StandardError; end
+
+# # A mock search engine
+# # that returns a random number instead of an actual count.
+# class SearchEngine
+#   def self.count(query, api_key)
+#     unless valid?(api_key)
+#       raise AuthenticationError, 'API key is not valid.'
+#     else
+#       rand(200_000)
+#     end
+#   end
+
+#   private
+
+#   def self.valid?(key)
+#     # puts "Keys are being compared... #{key}, LS1A"
+#     key == 'LS1A'
+#   end
+# end
+
+# module DoesItRock
+#   # API_KEY = 'LS1A'
+#   API_KEY = 'LS1A'
+
+#   class NoScore; end
+
+#   class Score
+#     def self.for_term(term)
+#       positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
+#       negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
+
+#       positive / (positive + negative)
+#     rescue ZeroDivisionError
+#       NoScore.new
+#     end
+#   end
+
+#   def self.find_out(term)
+#     score = Score.for_term(term)
+
+#     case score
+#     when NoScore
+#       "No idea about #{term}..."
+#     when 0...0.5
+#       "#{term} is not fun."
+#     when 0.5
+#       "#{term} seems to be ok..."
+#     else
+#       "#{term} rocks!"
+#     end
+#   rescue StandardError => e
+#     e.message
+#   end
+# end
+
+# # Example (your output may differ)
+
+# puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+# puts DoesItRock.find_out('Rain')        # Rain is not fun.
+# puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
+
+
+
+
+
+require 'pry'
+
+# A fake search engine
+# code omitted
 class AuthenticationError < Exception; end
 
 # A mock search engine
@@ -6,6 +76,7 @@ class SearchEngine
   def self.count(query, api_key)
     unless valid?(api_key)
       raise AuthenticationError, 'API key is not valid.'
+      binding.pry
     end
 
     rand(200_000)
@@ -19,7 +90,9 @@ class SearchEngine
 end
 
 module DoesItRock
-  API_KEY = 'LS1A'
+  # API_KEY = 'LS1A'
+
+  API_KEY = 'HELLO'
 
   class NoScore; end
 
